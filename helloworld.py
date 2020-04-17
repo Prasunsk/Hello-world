8b4396a6f90dcea3445bf192cce38aac91cb4860
Prasunsk <prasunsk@.github.com>
2020-04-18 01:43:21 +0530

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+def hello():                          
+    """A friendly function printing 'Hello World'
+    Hans Fangohr, for teaching Python, 15 Sept 2013 <fangohr@soton.ac.uk>
+    """
+    print("Hello World")
+    return None                        
+
+# main program starts
+hello()
